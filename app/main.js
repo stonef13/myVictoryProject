ded2b0bee54ff177b6b0621dbb48de5f264d3bfa
Stone Fuglaar <fuglaarusa@gmail.com>
2021-02-25 17:14:38 -0800

    Feature: Four charts of data

--- app/main.js
+++ app/main.js
@@ -8,39 +8,15 @@ import {
   VictoryTheme,
   VictoryStack,
   VictoryPie,
+  VictoryLine,
+  VictoryBrushContainer,
+  VictoryArea,
+  VictoryGroup,
 } from 'victory';
 
-function randomQuarterly() {
-  return Math.floor(Math.random() * 99000) + 20000;
-}
-
-// const data2017 = [
-//   { quarter: 1, earnings: randomQuarterly() },
-//   { quarter: 2, earnings: randomQuarterly() },
-//   { quarter: 3, earnings: randomQuarterly() },
-//   { quarter: 4, earnings: randomQuarterly() },
-// ];
-
-// const data2018 = [
-//   { quarter: 1, earnings: randomQuarterly() },
-//   { quarter: 2, earnings: randomQuarterly() },
-//   { quarter: 3, earnings: randomQuarterly() },
-//   { quarter: 4, earnings: randomQuarterly() },
-// ];
-
-// const data2019 = [
-//   { quarter: 1, earnings: randomQuarterly() },
-//   { quarter: 2, earnings: randomQuarterly() },
-//   { quarter: 3, earnings: randomQuarterly() },
-//   { quarter: 4, earnings: randomQuarterly() },
-// ];
-
-// const data2020 = [
-//   { quarter: 1, earnings: randomQuarterly() },
-//   { quarter: 2, earnings: randomQuarterly() },
-//   { quarter: 3, earnings: randomQuarterly() },
-//   { quarter: 4, earnings: randomQuarterly() },
-// ];
+// function randomQuarterly() {
+//   return Math.floor(Math.random() * 99000) + 20000;
+// }
 
 //COLORS
 // #264653
@@ -51,7 +27,22 @@ function randomQuarterly() {
 // #e63946
 // #6b705c
 
-const workHours = [
+const tikViews = [
+  // { x: 'date', y: # of views}
+  { x: '2020-6-28', y: 192 },
+  { x: '2020-9-20', y: 235 },
+  { x: '2020-9-20', y: 48 },
+  { x: '2020-10-25', y: 334 },
+  { x: '2020-10-28', y: 1755 },
+  { x: '2020-11-7', y: 97 },
+  { x: '2020-11-11', y: 2053 },
+  { x: '2020-11-21', y: 1171 },
+  { x: '2020-12-28', y: 291 },
+  { x: '2021-1-8', y: 305 },
+  { x: '2021-2-25', y: 1124 },
+];
+
+const workHoursChart = [
   { Monday: 1, hours: 12 },
   { Tuesday: 2, hours: 12 },
   { Wednesday: 3, hours: 12 },
@@ -59,6 +50,16 @@ const workHours = [
   { Friday: 5, hours: 11 },
   { Saturday: 6, hours: 8 },
   { Sunday: 7, hours: 2 },
+];
+
+const workHours = [
+  { x: 'M', y: 12 },
+  { x: 'T', y: 12 },
+  { x: 'W', y: 12 },
+  { x: 'T', y: 12 },
+  { x: 'F', y: 11 },
+  { x: 'S', y: 8 },
+  { x: 'S', y: 2 },
 ];
 
 const sleepHours = [
@@ -75,46 +76,73 @@ class Main extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hours per day working in bootcamp</h1>
-        <VictoryChart
-          singleQuadrantDomainPadding={{ x: false }}
-          domainPadding={100}
-          theme={VictoryTheme.material}
-        >
-          <VictoryAxis
-            tickValues={[0, 1, 2, 3, 4, 5, 6]}
-            tickFormat={['M', 'T', 'W', 'T', 'F', 'S', 'S']}
+        <div className='project-title'>
+          <p>Project Victory</p>
+          <p>by Stone Fuglaar</p>
+        </div>
+        <div className='container'>
+          {/* ANIMATIONS */}
+
+          <h1>Social Media Views</h1>
+          <VictoryLine
+            labels={({ datum }) => `${datum.y}`}
+            containerComponent={
+              <VictoryBrushContainer
+                brushDomain={{ x: [50, 100], y: [0, 100] }}
+                brushDimension='y'
+                brushStyle={{ fill: 'green', opacity: 0.2 }}
+              />
+            }
+            style={{
+              data: { stroke: 'black' },
+            }}
+            data={tikViews}
           />
-          <VictoryAxis
-            dependentAxis
-            // tickCount={8}
-            tickFormat={(x) => `${x} hrs`}
-          />
-          <VictoryStack>
-            {/* <VictoryBar data={data2017} x='quarter' y='earnings' />
-            <VictoryBar data={data2018} x='quarter' y='earnings' />
-            <VictoryBar data={data2019} x='quarter' y='earnings' />
-            <VictoryBar data={data2020} x='quarter' y='earnings' /> */}
-            <VictoryBar
-              data={workHours}
-              x='weekday'
-              y='hours'
-              // style={{
-              //   data: { fill: '#c43a31', stroke: 'black', strokeWidth: 2 },
-              // }}
+
+          {/* CHART */}
+
+          <h1>Hours per day working in bootcamp</h1>
+          <VictoryChart
+            singleQuadrantDomainPadding={{ x: false }}
+            domainPadding={100}
+            theme={VictoryTheme.material}
+          >
+            <VictoryAxis
+              tickValues={[0, 1, 2, 3, 4, 5, 6]}
+              tickFormat={['M', 'T', 'W', 'T', 'F', 'S', 'S']}
             />
-          </VictoryStack>
-        </VictoryChart>
+            <VictoryAxis dependentAxis tickFormat={(x) => `${x} hrs`} />
+            <VictoryStack>
+              <VictoryBar data={workHoursChart} x='weekday' y='hours' />
+            </VictoryStack>
+          </VictoryChart>
 
-        {/* POLAR CHART */}
+          {/* PIE */}
 
-        <h1>Hours Per Day Sleeping</h1>
-        <VictoryPie
-          data={sleepHours}
-          labels={({ datum }) => `${datum.x}: ${datum.y} hrs`}
-          padding={100}
-          theme={VictoryTheme.material}
-        />
+          <h1>Hours Per Day Sleeping</h1>
+          <VictoryPie
+            data={sleepHours}
+            labels={({ datum }) => `${datum.x}: ${datum.y} hrs`}
+            padding={100}
+            theme={VictoryTheme.material}
+          />
+
+          {/* VICTORY AREA  */}
+
+          <h1>Work and Sleep Hours</h1>
+          <h2 className='red'>Work = Red</h2>
+          <h2 className='yellow'>Sleep = Yellow</h2>
+          <VictoryChart width={400} height={400} theme={VictoryTheme.material}>
+            <VictoryGroup
+              style={{
+                data: { strokeWidth: 5, fillOpacity: 0.5 },
+              }}
+            >
+              <VictoryArea data={workHours} />
+              <VictoryArea data={sleepHours} />
+            </VictoryGroup>
+          </VictoryChart>
+        </div>
       </div>
     );
   }
